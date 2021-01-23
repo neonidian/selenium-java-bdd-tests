5f4b2f1c79ded7581f34525f39004527f591352e
Keerthi Shankar Subramanian <keerthi.subramanian@cepheid.com>
2021-01-23 23:00:06 +0100

    Add pages to modularize framework

--- src/test/java/com/mitigram/web/pages/HomePage.java
+++ src/test/java/com/mitigram/web/pages/HomePage.java
@@ -4,7 +4,7 @@ import com.mitigram.web.framework.LocatorObject;
 import com.mitigram.web.framework.browser.BrowserActions;
 
 public class HomePage {
-    BrowserActions browserActions;
+    private final BrowserActions browserActions;
     private static final String HOME_PAGE_NAME = "Home page ";
     private static final String HOME_PAGE_URL = "https://mitigram.com/";
     private static final LocatorObject LOGIN_LINK_HEADER = new LocatorObject("//header/nav[@class='right']//a[text()='Login']", LocatorObject.XPATH, HOME_PAGE_NAME + "> Header section > Login link");
@@ -14,18 +14,16 @@ public class HomePage {
         this.browserActions = browserActions;
     }
 
-    public com.mitigram.web.pages.HomePage goToMitigramHomePageByURL() {
+    public HomePage goToMitigramHomePageByURL() {
         browserActions.openURL(HOME_PAGE_URL);
         return this;
     }
 
-    public LoginPage navigateToLoginPageUsingHeader() {
+    public void navigateToLoginPageUsingHeader() {
         browserActions.click(LOGIN_LINK_HEADER);
-        return new LoginPage(browserActions);
     }
 
-    public LoginPage navigateToLoginPageUsingFooter() {
+    public void navigateToLoginPageUsingFooter() {
         browserActions.click(LOGIN_LINK_FOOTER);
-        return new LoginPage(browserActions);
     }
 }
--- src/test/java/com/mitigram/web/pages/LoginPage.java
+++ src/test/java/com/mitigram/web/pages/LoginPage.java
@@ -1,11 +1,12 @@
 package com.mitigram.web.pages;
 
+import com.mitigram.web.framework.LocatorObject;
 import com.mitigram.web.framework.browser.BrowserActions;
 
 public class LoginPage {
-    BrowserActions browserActions;
+    private final BrowserActions browserActions;
     private static final String LOGIN_PAGE_NAME = "Login page > ";
-    private static final com.mitigram.web.framework.LocatorObject LOGIN_DIV = new com.mitigram.web.framework.LocatorObject("div#login", com.mitigram.web.framework.LocatorObject.CSS, LOGIN_PAGE_NAME + "> Header section > Login link");
+    private static final LocatorObject LOGIN_DIV = new LocatorObject("div#login", LocatorObject.CSS, LOGIN_PAGE_NAME + "> Header section > Login link");
 
     public LoginPage(BrowserActions browserActions) {
         this.browserActions = browserActions;
