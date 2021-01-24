67e118244e5c5df2c5f82765e764569fd1b1cd0e
Keerthi Shankar Subramanian <keerthi.subramanian@cepheid.com>
2021-01-24 13:09:52 +0100

    Use association for running cucumber tests

--- src/test/java/com/mitigram/web/TestBase.java
+++ src/test/java/com/mitigram/web/TestBase.java
@@ -6,27 +6,41 @@ import com.mitigram.web.framework.browser.EdgeBrowser;
 import com.mitigram.web.framework.browser.FirefoxBrowser;
 import com.mitigram.web.pages.HomePage;
 import com.mitigram.web.pages.LoginPage;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.TestNGCucumberRunner;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
-abstract public class TestBase extends AbstractTestNGCucumberTests {
+abstract public class TestBase {
 
     private static final String CHROME = "CHROME";
     private static final String FIREFOX = "FIREFOX";
     private static final String EDGE = "EDGE";
     private static final List<String> supportedBrowsers = Arrays.asList(CHROME, FIREFOX, EDGE);
-    protected BrowserActions browserActions;
 
+    protected BrowserActions browserActions;
+    protected TestNGCucumberRunner testNGCucumberRunner;
     protected static HomePage homePage;
     protected static LoginPage loginPage;
+
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() {
+        testNGCucumberRunner.finish();
+    }
 
     @BeforeTest(alwaysRun = true)
     public void setupBrowser() {
@@ -35,10 +49,10 @@ abstract public class TestBase extends AbstractTestNGCucumberTests {
         initializePages();
     }
 
-     @AfterTest(alwaysRun = true)
-     public void exitBrowser() {
-         browserActions.exitBrowser();
-     }
+    @AfterTest(alwaysRun = true)
+    public void exitBrowser() {
+        browserActions.exitBrowser();
+    }
 
     private void startBrowser(String browser) {
         switch (browser) {
@@ -66,7 +80,7 @@ abstract public class TestBase extends AbstractTestNGCucumberTests {
         Configurations browserConfiguration = new Configurations();
         String browserToUse = null;
         try {
-            Configuration configuration = browserConfiguration.properties(new File("browser.properties"));
+            Configuration configuration = browserConfiguration.properties(new File("src/test/resources/com/mitigram/web/browser.properties"));
             browserToUse = configuration.getString("browserToUse").trim().toUpperCase();
         } catch (ConfigurationException configurationException) {
             configurationException.printStackTrace();
