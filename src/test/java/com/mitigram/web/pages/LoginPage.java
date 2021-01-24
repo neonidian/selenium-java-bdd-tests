18320219f358e84c0c51b245550508743c3de6cb
Keerthi Shankar Subramanian <keerthi.subramanian@cepheid.com>
2021-01-24 13:10:10 +0100

    Add new tests

--- src/test/java/com/mitigram/web/pages/LoginPage.java
+++ src/test/java/com/mitigram/web/pages/LoginPage.java
@@ -5,8 +5,9 @@ import com.mitigram.web.framework.browser.BrowserActions;
 
 public class LoginPage {
     private final BrowserActions browserActions;
+    private static final String LOGIN_PAGE_URL = "https://marketplace.mitigram.com/Account/Login";
     private static final String LOGIN_PAGE_NAME = "Login page > ";
-    private static final LocatorObject LOGIN_DIV = new LocatorObject("div#login", LocatorObject.CSS, LOGIN_PAGE_NAME + "> Header section > Login link");
+    private static final LocatorObject LOGIN_DIV = new LocatorObject("div#login", LocatorObject.CSS, LOGIN_PAGE_NAME + "> Login div");
 
     public LoginPage(BrowserActions browserActions) {
         this.browserActions = browserActions;
@@ -14,5 +15,10 @@ public class LoginPage {
 
     public boolean isLoginPageDisplayed() {
         return browserActions.isElementVisibleOnPage(LOGIN_DIV);
+    }
+
+    public LoginPage goToLoginPageByURL() {
+        browserActions.openURL(LOGIN_PAGE_URL);
+        return this;
     }
 }
--- src/test/java/com/mitigram/web/stepdefinitions/LoginPageStepDefinitions.java
+++ src/test/java/com/mitigram/web/stepdefinitions/LoginPageStepDefinitions.java
@@ -2,6 +2,7 @@ package com.mitigram.web.stepdefinitions;
 
 import com.mitigram.web.TestBase;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -11,5 +12,10 @@ public class LoginPageStepDefinitions extends TestBase {
     @Then("the login page is displayed")
     public void verifyIfLoginPageIsDisplayed() {
         assertThat(loginPage.isLoginPageDisplayed(), is(true));
+    }
+
+    @When("user uses the login URL in the browser")
+    public void userUsesTheLoginURLInTheBrowser() {
+        loginPage.goToLoginPageByURL();
     }
 }
